e128201bc79ae4cb74a0641036d3e6682cdfc805
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-21 13:58:24 -0500

    Create the test cases for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,6 +1,27 @@
-describe "Solver" do
-  context "#factorial" do
-    result = solver.factorial(5)
+require_relative '../solver'
+
+describe Solver do
+  before :all do
+    @solver = Solver.new
   end
-  expect result.to eq(120)
+
+  context "#factorial method" do
+    it 'throws an argument error if the input parameter is a negative integer' do
+      expect { @solver.factorial(-5) }.to raise_exception ArgumentError
+    end
+
+    it 'throws an argument error if the input parameter is a string' do
+      expect { @solver.factorial('5') }.to raise_exception ArgumentError
+    end
+
+    it 'returns the factorial of a positive integer' do
+      factorial = @solver.factorial(5)
+      expect(factorial).to eql 120
+    end
+
+    it 'returns the factorial of zero' do
+      factorial = @solver.factorial(0)
+      expect(factorial).to eql 1
+    end
+  end
 end
