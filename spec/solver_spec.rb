a781d2201f54978b1be6e6d097ff21fea9c0b6d1
Fabricio Garcia <fabgrel10@gmail.com>
2022-04-21 14:20:50 -0500

    Add tests to reverse mehtod

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,7 +5,7 @@ describe Solver do
     @solver = Solver.new
   end
 
-  context "#factorial method" do
+  context "#factorial" do
     it 'throws an argument error if the input parameter is a negative integer' do
       expect { @solver.factorial(-5) }.to raise_exception ArgumentError
     end
@@ -22,6 +22,24 @@ describe Solver do
     it 'returns the factorial of zero' do
       factorial = @solver.factorial(0)
       expect(factorial).to eql 1
+    end
+  end
+
+  context 'reverse' do
+    reversed = nil
+    it 'returns "54321" when argument is "12345"' do
+      reversed = @solver.reverse('12345')
+      expect(reversed).to eql '54321'
+    end
+
+    it 'correctly reverses a sentence' do
+      reversed = @solver.reverse('My sentence')
+      expect(reversed).to eql 'ecnetnes yM'
+    end
+
+    it "throws an error if there's more than 1 argument" do
+      reversed = @solver.reverse('a', 'b')
+      expect(reversed).to raise_exception ArgumentError
     end
   end
 end
