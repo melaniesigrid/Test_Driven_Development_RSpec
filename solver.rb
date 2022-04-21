1bac992d54ad165ca29b2dd5d7c3da7324b79080
Melanie Arellano <melaniesigrid@protonmail.com>
2022-04-21 14:09:12 -0600

    Create fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -8,4 +8,16 @@ class Solver
   def reverse(string)
     string.reverse
   end
+
+  def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+      "fizzbuzz"
+    elsif num % 3 == 0
+      "fizz"
+    elsif num % 5 == 0
+      "buzz"
+    else
+      num.to_s
+    end
+  end
 end
